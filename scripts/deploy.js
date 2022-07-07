5ea3e9663724707e636ee4063ad0e7153ea901c2
Anmol Sen <105110161+Anmo1Sen@users.noreply.github.com>
2022-07-07 12:44:28 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+
+
+
+    //0x330Efe1a3A7383136B825A247cE0a2C438d47BeD
